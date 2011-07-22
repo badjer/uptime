6a606655d48347aa15989dcffff5cf3860ca013d
badjer <brian.dejong@gmail.com>
2011-07-22 14:44:53 -0700

    Pull from envvar

--- src/uptime/server.clj
+++ src/uptime/server.clj
@@ -3,9 +3,14 @@
 
 (server/load-views "src/uptime/views/")
 
+(defn env [name]
+  (get (System/getenv) name))
+
+(def DB_SERVER (env "CLOUDANT_URL"))
+
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer/parseInt (or (get (System/getenv) "PORT") "8080"))]
+        port (Integer/parseInt (or (env "PORT") "8080"))]
     (server/start port {:mode mode
                         :ns 'uptime})))
 
